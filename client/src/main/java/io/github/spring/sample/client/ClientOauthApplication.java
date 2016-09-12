8b1951ad6d96b3f64752b148d4f0aa47e08a3679
Fernando Nogueira <fernando.a.gomes.n@gmail.com>
2016-09-12 15:30:57 -0300

    initial commit

---
+++ client/src/main/java/io/github/spring/sample/client/ClientOauthApplication.java
@@ -0,0 +1,12 @@
+package io.github.spring.sample.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientOauthApplication.class, args);
+	}
+}
